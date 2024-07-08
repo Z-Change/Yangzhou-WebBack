172aa6948ea458384a95667bd75d48e04ccd653b
wutcriminal <1668139313@qq.com>
2024-07-08 14:48:24 +0800

    'init'

---
+++ newbee-mall-api-spring-boot-3.x/src/main/java/ltd/newbee/mall/api/mall/vo/NewBeeMallShoppingCartItemVO.java
@@ -0,0 +1,39 @@
+/**
+ * 严肃声明：
+ * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
+ * 本软件已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
+ * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
+ * Copyright (c) 2019-2021 十三 all rights reserved.
+ * 版权所有，侵权必究！
+ */
+package ltd.newbee.mall.api.mall.vo;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 购物车页面购物项VO
+ */
+@Data
+public class NewBeeMallShoppingCartItemVO implements Serializable {
+
+    @Schema(title = "购物项id")
+    private Long cartItemId;
+
+    @Schema(title = "商品id")
+    private Long goodsId;
+
+    @Schema(title = "商品数量")
+    private Integer goodsCount;
+
+    @Schema(title = "商品名称")
+    private String goodsName;
+
+    @Schema(title = "商品图片")
+    private String goodsCoverImg;
+
+    @Schema(title = "商品价格")
+    private Integer sellingPrice;
+}
