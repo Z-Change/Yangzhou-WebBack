bf55d5781fca2e4ec2e45ac44966665139835dad
wutcriminal <1668139313@qq.com>
2024-07-17 14:02:21 +0800

    删除一些原住民

--- pilipili-mall-api-spring-boot-3.x/src/main/java/whut/pilipili/mall/PilipiliMallAPIApplication.java
+++ pilipili-mall-api-spring-boot-3.x/src/main/java/whut/pilipili/mall/PilipiliMallAPIApplication.java
@@ -1,6 +1,7 @@
 
 package whut.pilipili.mall;
 
+import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @MapperScan("whut.pilipili.mall.dao")
 @SpringBootApplication
+@EnableNacosDiscovery
 public class PilipiliMallAPIApplication {
 
     public static void main(String[] args) {
